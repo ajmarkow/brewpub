0dad85a98a6850f8e09af0bb317876e9904325d2
AJ Markow <alexanderjmarkow@gmail.com>
2020-11-20 10:21:44 -0800

    Install Tailwind CSS Framework and PostCSS. Add Header Component.

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  const name = "The Brewpub"
+  const myStyle={
+    fontFamily: 'Alfa Slab One'
+
+  }
+  return(
+    <React.Fragment>
+        <div class="self-center w-80 p-4 text-white bg-purple-600 rounded-md shadow-xs">
+          <h3 style={myStyle}>{name}</h3>
+        </div>
+    </React.Fragment>
+  );
+}
+
+export default Header;
