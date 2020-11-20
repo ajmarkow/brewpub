0297879202b1720bf4031bc87ffc64ec18c5800d
AJ Markow <alexanderjmarkow@gmail.com>
2020-11-20 10:30:20 -0800

    Remove unused logo.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-        <Header />
+      <div class="flex h-screen">
+        <div class="m-auto">
+          <Header />
+        </div>
+      </div>
     </div>
   );
 }
