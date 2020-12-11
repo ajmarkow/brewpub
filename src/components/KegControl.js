a9d8cdc43c5498cb841518f751ddf4558fd31b02
AJ Markow <alexanderjmarkow@gmail.com>
2020-12-10 16:32:36 -0800

    Fix statement to evaluate for clicking decrement.

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -4,99 +4,120 @@ import KegList from "./KegList";
 import KegDetail from "./KegDetail";
 
 class KegControl extends React.Component {
-
-    constructor(props) {
-      super(props);
-      this.state = {
-        formVisible: false,
-        fullListOfBrews: [],
-        currentBrew: null,
-        count:124
-      };
-      this.handleKegClick = this.handleKegClick.bind(this);
-    }
-
-    handleKegClick = () => {
-  if (this.state.currentBrew != null) {
-    this.setState({
+  constructor(props) {
+    super(props);
+    this.state = {
       formVisible: false,
-      currentBrew: null
-    });  
-  } else {
-    this.setState(prevState => ({formVisible:!prevState.formVisible
-    }));
-  }
-}
-
-    handleClickingIncrement = (id) => {
-      if (individualKeg.fluidOunces <= 124) {
-        const individualKeg = this.state.fullListOfBrews.filter(brew => brew.id === id)[0];
-        individualKeg.fluidOunces += 1
-        const fullListWithQuantityUpdate = this.state.fullListOfBrews.filter(brew => brew.id !== id).concat(individualKeg)  
-      this.setState({
-        fullListOfBrews: fullListWithQuantityUpdate
-      })
-    }
-  }
-    handleClickingDecrement = (id) => {
-      if (individualKeg.fluidOunces > 0) {
-        const individualKeg = this.state.fullListOfBrews.filter(brew => brew.id === id)[0];
-        individualKeg.fluidOunces -= 1
-        const fullListWithQuantityUpdate = this.state.fullListOfBrews.filter(brew => brew.id !== id).concat(individualKeg)  
-      this.setState({
-        fullListOfBrews:fullListWithQuantityUpdate
-      });    
-    }
+      fullListOfBrews: [],
+      currentBrew: null,
+      count: 124,
+    };
+    this.handleKegClick = this.handleKegClick.bind(this);
   }
 
-    handleUpdatingCurrentBrew = (id) => {
-      const currentBrew= this.state.fullListOfBrews.filter(brew => brew.id === id)[0];
-      this.setState({currentBrew: currentBrew})
-    }
-
-    addNewBrew = (addedBrew) => {
-      const updatedFullListOfBrews = this.state.fullListOfBrews.concat(addedBrew)
+  handleKegClick = () => {
+    if (this.state.currentBrew != null) {
       this.setState({
-        fullListOfBrews: updatedFullListOfBrews,
         formVisible: false,
-      })
+        currentBrew: null,
+      });
+    } else {
+      this.setState((prevState) => ({ formVisible: !prevState.formVisible }));
     }
+  };
 
-    removeBrew = (id) => {
-      const newFullListofBrews = this.state.fullListOfBrews.filter(keg => keg.id !== id);
+  handleClickingIncrement = (id) => {
+    if (individualKeg.fluidOunces <= 124) {
+      const individualKeg = this.state.fullListOfBrews.filter(
+        (brew) => brew.id === id
+      )[0];
+      individualKeg.fluidOunces += 1;
+      const fullListWithQuantityUpdate = this.state.fullListOfBrews
+        .filter((brew) => brew.id !== id)
+        .concat(individualKeg);
       this.setState({
-        fullListOfBrews: newFullListofBrews,
-        currentBrew: null
+        fullListOfBrews: fullListWithQuantityUpdate,
       });
     }
-    
-    render(){
-      let selectedVisibleState = null;
-      let buttonText = null;
-
-      if (this.state.currentBrew != null) {
-        selectedVisibleState = <KegDetail keg={this.state.currentBrew} onClickingDelete={this.removeBrew} onClickingDecrement={ this.handleClickingDecrement} onClickingIncrement={this.handleClickingIncrement}/>
-        buttonText= "Back to Full Brew Selection"
-      }
-
-      else if (this.state.formVisible) {
-        selectedVisibleState = <AddKegForm onSubmit={this.addNewBrew} />
-        buttonText="See our Selection of Brews"
-      } else {
-        selectedVisibleState = <KegList kegList={this.state.fullListOfBrews} onBrewSelection={this.handleUpdatingCurrentBrew}/>;
-        buttonText="Add a New Brew"
-
+  };
+  handleClickingDecrement = (id) => {
+    if (individualKeg.fluidOunces >= 1) {
+      const individualKeg = this.state.fullListOfBrews.filter(
+        (brew) => brew.id === id
+      )[0];
+      individualKeg.fluidOunces -= 1;
+      const fullListWithQuantityUpdate = this.state.fullListOfBrews
+        .filter((brew) => brew.id !== id)
+        .concat(individualKeg);
+      this.setState({
+        fullListOfBrews: fullListWithQuantityUpdate,
+      });
     }
-    return(
+  };
+
+  handleUpdatingCurrentBrew = (id) => {
+    const currentBrew = this.state.fullListOfBrews.filter(
+      (brew) => brew.id === id
+    )[0];
+    this.setState({ currentBrew: currentBrew });
+  };
+
+  addNewBrew = (addedBrew) => {
+    const updatedFullListOfBrews = this.state.fullListOfBrews.concat(addedBrew);
+    this.setState({
+      fullListOfBrews: updatedFullListOfBrews,
+      formVisible: false,
+    });
+  };
+
+  removeBrew = (id) => {
+    const newFullListofBrews = this.state.fullListOfBrews.filter(
+      (keg) => keg.id !== id
+    );
+    this.setState({
+      fullListOfBrews: newFullListofBrews,
+      currentBrew: null,
+    });
+  };
+
+  render() {
+    let selectedVisibleState = null;
+    let buttonText = null;
+
+    if (this.state.currentBrew != null) {
+      selectedVisibleState = (
+        <KegDetail
+          keg={this.state.currentBrew}
+          onClickingDelete={this.removeBrew}
+          onClickingDecrement={this.handleClickingDecrement}
+          onClickingIncrement={this.handleClickingIncrement}
+        />
+      );
+      buttonText = "Back to Full Brew Selection";
+    } else if (this.state.formVisible) {
+      selectedVisibleState = <AddKegForm onSubmit={this.addNewBrew} />;
+      buttonText = "See our Selection of Brews";
+    } else {
+      selectedVisibleState = (
+        <KegList
+          kegList={this.state.fullListOfBrews}
+          onBrewSelection={this.handleUpdatingCurrentBrew}
+        />
+      );
+      buttonText = "Add a New Brew";
+    }
+    return (
       <React.Fragment>
         {selectedVisibleState}
-        <button className ='px-9 py-6 bg-black text-white active:bg-red-600 font-bold rounded shadow-xl hover: bg-red-500 text-white active:bg-red-600 font-bold rounded shadow-xl' onClick={this.handleKegClick}>{buttonText}</button>
+        <button
+          className="px-9 py-6 bg-black text-white active:bg-red-600 font-bold rounded shadow-xl hover: bg-red-500 text-white active:bg-red-600 font-bold rounded shadow-xl"
+          onClick={this.handleKegClick}
+        >
+          {buttonText}
+        </button>
       </React.Fragment>
     );
   }
-
-
-
-  }
+}
 
 export default KegControl;
